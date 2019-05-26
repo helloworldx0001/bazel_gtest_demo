887bafee3bef6a4dc326e397e356e0948574c82a
helloworldx0001 <milkywaysss@qq.com>
2019-05-26 15:02:03 +0800

    test

---
+++ test/test_main.cc
@@ -0,0 +1,15 @@
+#include <fstream>
+#include <memory>
+#include <string>
+#include <vector>
+
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+
+using std::vector;
+
+TEST(TestCaseName, TestName) {
+	EXPECT_EQ (1, 1); 
+}
+
+
